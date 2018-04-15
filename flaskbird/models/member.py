455c434ce3b9349644cb09d9bbcc4653a8287803
Shingo Yamada <uzuranoie@gmail.com>
2018-04-15 09:33:18 +0900

    Added avater image on member page

--- flaskbird/models/member.py
+++ flaskbird/models/member.py
@@ -1,5 +1,6 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
+from hashlib import md5
 from flask_login import UserMixin
 from flaskbird.database import db
 from .base import Base
@@ -19,3 +20,6 @@ class Member(UserMixin, Base):
     def check_password(self, password):
         return check_password_hash(self.password, password)
 
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
