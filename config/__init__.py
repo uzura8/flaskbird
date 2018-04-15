4a2563315989961f67762056e06bd54acd99606a
Shingo Yamada <uzuranoie@gmail.com>
2018-04-15 15:15:42 +0900

    Implemented loggin

--- config/__init__.py
+++ config/__init__.py
@@ -11,3 +11,14 @@ class BaseConfig(object):
     DEBUG = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
+
+    FBD_ADMIN_MAIL = 'admin@example.com'
+
+    IS_DEBUG_LOGGING = False
+    IS_SEND_ERROR_REPORT_MAIL = False
+    MAIL_SERVER = None
+    MAIL_PORT = 25
+    MAIL_USE_TLS = None
+    MAIL_USERNAME = None
+    MAIL_PASSWORD = None
+    ADMINS = None
--- flaskbird/__init__.py
+++ flaskbird/__init__.py
@@ -1,8 +1,10 @@
+import os
+import logging
+from logging.handlers import SMTPHandler
+from logging.handlers import RotatingFileHandler
 from flask import Flask
 from flask_login import LoginManager
 from flaskbird.database import init_db
-import flaskbird.models
-from flaskbird.models import Member
 
 from .views.site import site
 from .views.member import member
@@ -25,6 +27,8 @@ def create_app():
 app = create_app()
 login = LoginManager(app)
 
+import flaskbird.models
+from flaskbird.models import Member
 from .views import error
 
 @login.user_loader
@@ -35,3 +39,31 @@ modules = [site, member]
 for module in modules:
     app.register_blueprint(module)
 
+if app.config['IS_SEND_ERROR_REPORT_MAIL']:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['FBD_ADMIN_MAIL'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+if app.config['IS_DEBUG_LOGGING']:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Flaskbird startup')
